e0075194bf83792cad07fbc4facb7a1c88289ff4
Md. Mehedi Hasan <bsse0607@iit.du.ac.bd>
2018-10-30 18:52:56 +0600

    color choice fix on add to cart modal

--- resources/views/frontend/partials/addToCart.blade.php
+++ resources/views/frontend/partials/addToCart.blade.php
@@ -88,8 +88,8 @@
                             <ul class="list-inline checkbox-color mb-1">
                                 @foreach (json_decode($product->colors) as $key => $color)
                                     <li>
-                                        <input type="radio" id="color-{{ $key }}" name="color" value="{{ $color }}" <?php if($key == 0) echo "checked";?> >
-                                        <label style="background: {{ $color }};" for="color-{{ $key }}" data-toggle="tooltip"></label>
+                                        <input type="radio" id="{{ $product->id }}-color-{{ $key }}" name="color" value="{{ $color }}" <?php if($key == 0) echo "checked";?> >
+                                        <label style="background: {{ $color }};" for="{{ $product->id }}-color-{{ $key }}" data-toggle="tooltip"></label>
                                     </li>
                                 @endforeach
                             </ul>

c4938ff5905a3df9bb0b28e817c901e1553f384e
Unknown <santuxroy@gmail.com>
2018-11-13 17:19:52 +0600

    footer update, custom-style update

--- resources/views/frontend/inc/footer.blade.php
+++ resources/views/frontend/inc/footer.blade.php
@@ -1,168 +1,158 @@
+
+<section class="slice-sm footer-top-bar">
+    <div class="container sct-inner">
+        <div class="row no-gutters">
+            <div class="col-lg-3">
+                <div class="footer-top-box text-center">
+                    <a href="">
+                        <i class="fa fa-tv"></i>
+                        <h4 class="heading-5">Seller Policy</h4>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="footer-top-box text-center">
+                    <a href="">
+                        <i class="fa fa-tv"></i>
+                        <h4 class="heading-5">Return Policy</h4>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="footer-top-box text-center">
+                    <a href="">
+                        <i class="fa fa-tv"></i>
+                        <h4 class="heading-5">Support Policy</h4>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="footer-top-box text-center">
+                    <a href="">
+                        <i class="fa fa-tv"></i>
+                        <h4 class="heading-5">My Profile</h4>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 <!-- FOOTER -->
-<footer id="footer" class="footer footer-inverse">
+<footer id="footer" class="footer">
     <div class="footer-top">
         <div class="container">
             <div class="row cols-xs-space cols-sm-space cols-md-space">
-                <div class="col-md-4 col-lg-2">
-                    <div class="col">
-                        <h4 class="heading heading-xs strong-600 text-uppercase mb-1">
-                            Main menu
-                        </h4>
 
+                <div class="col-md-4 col-lg-4">
+                    <div class="col">
+                        <a href="#" class="d-block">
+                            <img src="{{ asset('frontend/images/logo/logo.svg') }}" class="" height="44">
+                        </a>
+                        <p class="mt-3">Active Super Shop Multi vendor system is such a platform to build a border less marketplace both for physical and digital goods.</p>
+                        <div>
+                            <form class="form-inline">
+                                <div class="form-group">
+                                    <input type="email" class="form-control" placeholder="Your Email Address">
+                                </div>
+                                <button type="submit" class="btn btn-base-1 btn-icon-left">
+                                    Subscribe
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 offset-lg-1">
+                    <div class="col text-center text-md-left">
+                        <h4 class="heading heading-xs strong-600 text-uppercase mb-2">
+                            Contact Info
+                        </h4>
+                        <ul class="footer-links contact-widget">
+                            <li>
+                               <span class="d-block opacity-5">Address:</span>
+                               <span class="d-block">1234 Street Name, City, England</span>
+                            </li>
+                            <li>
+                               <span class="d-block opacity-5">Phone:</span>
+                               <span class="d-block">Toll Free (123) 456-7890</span>
+                            </li>
+                            <li>
+                               <span class="d-block opacity-5">Email:</span>
+                               <span class="d-block">
+                                   <a href="mailto:mail@example.com">mail@example.com</a>
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="col text-center text-md-left">
+                        <h4 class="heading heading-xs strong-600 text-uppercase mb-2">
+                            Useful Link
+                        </h4>
                         <ul class="footer-links">
                             <li>
+                                <a href="#" title="">
+                                    Link 1
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="">
+                                    Link 2
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="">
+                                    Link 3
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="">
+                                    Link 4
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="">
+                                    Link 5
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-lg-2">
+                    <div class="col text-center text-md-left">
+                       <h4 class="heading heading-xs strong-600 text-uppercase mb-2">
+                          My Account
+                       </h4>
+
+                       <ul class="footer-links">
+                            <li>
                                 <a href="#" title="Home">
-                                    Home
+                                    Login
                                 </a>
                             </li>
                             <li>
                                 <a href="#" title="About us">
-                                    About us
+                                    Order History
                                 </a>
                             </li>
                             <li>
                                 <a href="#" title="Services">
-                                    Services
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" title="Blog">
-                                    Blog
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" title="Contact">
-                                    Contact
+                                    My Wishlist
                                 </a>
                             </li>
                         </ul>
                     </div>
-                </div>
-
-                <div class="col-md-4 col-lg-2">
-                    <div class="col">
-                        <h4 class="heading heading-xs strong-600 text-uppercase mb-1">
-                            Help and support
-                        </h4>
-
-                        <ul class="footer-links">
-                            <li>
-                                <a href="#" title="Help center">
-                                    Help center
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" title="Discussions">
-                                    Discussions
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" title="Contact support">
-                                    Contact support
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" title="Knowledge center">
-                                    Knowledge center
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" title="Jobs">
-                                    FAQ
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-md-4 col-lg-2">
-                    <div class="col">
-                        <h4 class="heading heading-xs strong-600 text-uppercase mb-1">
-                            Our services
-                        </h4>
-
-                        <ul class="footer-links">
-                            <li>
-                                <a href="#" title="Help center">
-                                    Concept
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" title="Discussions">
-                                    UI/UX design
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" title="Contact support">
-                                    Web developement
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" title="Knowledge center">
-                                    Custom solutions
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" title="Jobs">
-                                    Branding
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-                <span class="space-sm-only-3"></span>
-
-                <div class="col-md-4 col-lg-3">
-                    <div class="col">
-                        <h4 class="heading heading-xs strong-600 text-uppercase mb-1">
-                            From the blog
-                        </h4>
-
-                        <ul class="footer-links">
-                            <li>
-                                <a href="#" title="">Clamtown weekly news</a>
-                                <small class="footer-link-date">June 20, 2018</small>
-                            </li>
-                            <li class="">
-                                <a href="#" title="">Fall in love with Boomerang</a>
-                                <small class="footer-link-date">June 18, 2018</small>
-                            </li>
-                            <li class="">
-                                <a href="#" title="">Cracking the Boomerang code</a>
-                                <small class="footer-link-date">March 30, 2018</small>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-md-4 col-lg-3">
-                    <div class="col">
-                        <h4 class="heading heading-xs strong-600 text-uppercase mb-1">
-                            Check our feed
-                        </h4>
-
-                        <div class="photostream">
-                            <span class="photo-wrapper">
-                                <a href="{{ asset('frontend/images/prv/blog/img-1-200x200.jpg') }}" data-fancybox>
-                                    <img src="{{ asset('frontend/images/prv/blog/img-1-200x200.jpg') }}">
-                                </a>
-                            </span>
-                            <span class="photo-wrapper">
-                                <a href="{{ asset('frontend/images/prv/blog/img-2-200x200.jpg') }}" data-fancybox>
-                                    <img src="{{ asset('frontend/images/prv/blog/img-2-200x200.jpg') }}">
-                                </a>
-                            </span>
-                            <span class="photo-wrapper">
-                                <a href="{{ asset('frontend/images/prv/blog/img-3-200x200.jpg') }}" data-fancybox>
-                                    <img src="{{ asset('frontend/images/prv/blog/img-3-200x200.jpg') }}">
-                                </a>
-                            </span>
-                            <span class="photo-wrapper">
-                                <a href="{{ asset('frontend/images/prv/blog/img-4-200x200.jpg') }}" data-fancybox>
-                                    <img src="{{ asset('frontend/images/prv/blog/img-4-200x200.jpg') }}">
-                                </a>
-                            </span>
+                    <div class="col text-center text-md-left">
+                        <div class="mt-4">
+                            <h4 class="heading heading-xs strong-600 text-uppercase mb-2">
+                                Be a Seller
+                            </h4>
+                            <button type="button" class="btn btn-base-1 btn-icon-left">
+                                Apply Now
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -170,39 +160,62 @@
         </div>
     </div>
 
-    <div class="footer-bottom">
+    <div class="footer-bottom py-3 sct-color-3">
         <div class="container">
             <div class="row row-cols-xs-spaced flex flex-items-xs-middle">
-                <div class="col col-sm-7 col-xs-12">
+                <div class="col col-sm-4 col-xs-12">
                     <div class="copyright text-xs-center text-sm-left">
-                        Copyright &copy; 2018                        <a href="http://webpixels.io" target="_blank" title="Webpixels - Official Website">
-                            <strong class="strong-400">Webpixels</strong>
-                        </a> -
-                        All rights reserved
+                        <ul class="copy-links no-margin">
+                            <li>
+                                © 2018 Active Super Shop
+                            </li>
+                            <li>
+                                <a href="#">Terms</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy policy</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-                <div class="col col-sm-5 col-xs-12">
+                <div class="col col-sm-4 col-xs-12">
+                    <ul class="social-media social-media--style-1-v4 text-center">
+                        <li>
+                            <a href="#" class="facebook" target="_blank" data-toggle="tooltip" data-original-title="Facebook">
+                                <i class="fa fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="instagram" target="_blank" data-toggle="tooltip" data-original-title="Instagram">
+                                <i class="fa fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="dribbble" target="_blank" data-toggle="tooltip" data-original-title="Dribbble">
+                                <i class="fa fa-dribbble"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="dribbble" target="_blank" data-toggle="tooltip" data-original-title="Github">
+                                <i class="fa fa-github"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col col-sm-4 col-xs-12">
                     <div class="text-xs-center text-sm-right">
-                        <ul class="social-media social-media--style-1-v4">
+                        <ul class="inline-links">
                             <li>
-                                <a href="#" class="facebook" target="_blank" data-toggle="tooltip" data-original-title="Facebook">
-                                    <i class="fa fa-facebook"></i>
-                                </a>
+                                <img src="{{ asset('frontend/images/icons/cards/visa.png') }}" width="30" class="img-grayscale">
                             </li>
                             <li>
-                                <a href="#" class="instagram" target="_blank" data-toggle="tooltip" data-original-title="Instagram">
-                                    <i class="fa fa-instagram"></i>
-                                </a>
+                                <img src="{{ asset('frontend/images/icons/cards/mastercard.png') }}" width="30" class="img-grayscale">
                             </li>
                             <li>
-                                <a href="#" class="dribbble" target="_blank" data-toggle="tooltip" data-original-title="Dribbble">
-                                    <i class="fa fa-dribbble"></i>
-                                </a>
+                                <img src="{{ asset('frontend/images/icons/cards/maestro.png') }}" width="30" class="img-grayscale">
                             </li>
                             <li>
-                                <a href="#" class="dribbble" target="_blank" data-toggle="tooltip" data-original-title="Github">
-                                    <i class="fa fa-github"></i>
-                                </a>
+                                <img src="{{ asset('frontend/images/icons/cards/paypal.png') }}" width="30" class="img-grayscale">
                             </li>
                         </ul>
                     </div>

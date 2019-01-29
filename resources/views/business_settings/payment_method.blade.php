eb6a0cd39a0dcd4214ece65ed50af68da7618d03
Naimcse56 <naimcse56@gmail.com>
2019-01-29 18:02:49 +0600

    stripe config added on admin panel

--- resources/views/business_settings/payment_method.blade.php
+++ resources/views/business_settings/payment_method.blade.php
@@ -102,6 +102,43 @@
             </div>
         </div>
     </div>
+
+    <div class="col-lg-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <h3 class="panel-title text-center">{{__('Stripe Credential')}}</h3>
+            </div>
+            <div class="panel-body">
+                <form class="form-horizontal" action="{{ route('payment_method.update') }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="payment_method" value="sslcommerz">
+                    <div class="form-group">
+                        <input type="hidden" name="types[]" value="STRIPE_STORE_ID">
+                        <div class="col-lg-3">
+                            <label class="control-label">{{__('STRIPE KEY')}}</label>
+                        </div>
+                        <div class="col-lg-6">
+                            <input type="text" class="form-control" name="STRIPE_KEY" value="{{  env('STRIPE_KEY') }}" placeholder="STRIPE KEY" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="hidden" name="types[]" value="STRIPE_SECRET">
+                        <div class="col-lg-3">
+                            <label class="control-label">{{__('STRIPE SECRET')}}</label>
+                        </div>
+                        <div class="col-lg-6">
+                            <input type="text" class="form-control" name="STRIPE_SECRET" value="{{  env('STRIPE_SECRET') }}" placeholder="STRIPE SECRET" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-lg-12 text-right">
+                            <button class="btn btn-purple" type="submit">{{__('save')}}</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection

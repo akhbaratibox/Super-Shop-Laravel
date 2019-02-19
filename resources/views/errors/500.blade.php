a2dc118a9c9ac5af5bb2b841cd49742ab0485827
Unknown <santuxroy@gmail.com>
2019-02-19 16:23:40 +0600

    500 added

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="text-center">
-    <h1 class="error-code text-danger">{{__('404')}}</h1>
-    <p class="h4 text-uppercase text-bold">{{__('Page Not Found!')}}</p>
+    <h1 class="error-code text-danger">{{__('500')}}</h1>
+    <p class="h4 text-uppercase text-bold">{{__('OOPS!')}}</p>
     <div class="pad-btm">
-        {{__('Sorry, but the page you are looking for has not been found on our server.')}}
+        {{__('Something went wrong. Looks like server failed to load your request.')}}
     </div>
     <hr class="new-section-sm bord-no">
     <div class="pad-top"><a class="btn btn-primary" href="{{env('APP_URL')}}">{{__('Return Home')}}</a></div>

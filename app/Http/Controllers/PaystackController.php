c23713defd58fb0d25e68a95205992bb3ccd5047
Mehedi <mehedi.iitdu@gmail.com>
2019-07-23 16:44:36 +0600

    paystack updated

--- app/Http/Controllers/PaystackController.php
+++ app/Http/Controllers/PaystackController.php
@@ -7,7 +7,7 @@ use App\Http\Requests;
 use App\Order;
 use App\Seller;
 use App\Http\Controllers\Controller;
-use Unicodeveloper\Paystack\Paystack;
+use Mehedi\Paystack\Paystack;
 use Auth;
 use Session;
 use Redirect;
@@ -16,32 +16,34 @@ class PaystackController extends Controller
 {
     public function redirectToGateway(Request $request)
     {
+        $baseUrl = "https://api.paystack.co";
+
         if(Session::get('payment_type') == 'cart_payment'){
             $order = Order::findOrFail(Session::get('order_id'));
-            $paystack = new Paystack();
+            $paystack = new Paystack(env('PAYSTACK_SECRET_KEY'), $baseUrl);
             $user = Auth::user();
             $request->email = $user->email;
             $request->amount = round(convert_to_usd($order->grand_total) * 100);
-            $request->key = env('PAYSTACK_SECRET_KEY');
+            //$request->key = env('PAYSTACK_SECRET_KEY');
             $request->reference = $paystack->genTranxRef();
             return $paystack->getAuthorizationUrl()->redirectNow();
         }
         elseif(Session::get('payment_type') == 'seller_payment'){
             $seller = Seller::findOrFail(Session::get('payment_data')['seller_id']);
-            $paystack = new Paystack();
+            $paystack = new Paystack($seller->paystack_secret_key, $baseUrl);
             $user = Auth::user();
             $request->email = $user->email;
             $request->amount = round(convert_to_usd($request->session()->get('payment_data')['amount']) * 100);
-            $request->key = $seller->paystack_secret_key;
+            // $request->key = $seller->paystack_secret_key;
             $request->reference = $paystack->genTranxRef();
             return $paystack->getAuthorizationUrl()->redirectNow();
         }
         elseif(Session::get('payment_type') == 'wallet_payment'){
-            $paystack = new Paystack();
+            $paystack = new Paystack(env('PAYSTACK_SECRET_KEY'), $baseUrl);
             $user = Auth::user();
             $request->email = $user->email;
             $request->amount = round(convert_to_usd($request->session()->get('payment_data')['amount']) * 100);
-            $request->key = env('PAYSTACK_SECRET_KEY');
+            // $request->key = env('PAYSTACK_SECRET_KEY');
             $request->reference = $paystack->genTranxRef();
             return $paystack->getAuthorizationUrl()->redirectNow();
         }
@@ -54,25 +56,30 @@ class PaystackController extends Controller
      */
     public function handleGatewayCallback()
     {
-        $paystack = new Paystack();
-
-        $payment = $paystack->getPaymentData();
-
-        $payment_detalis = json_encode($payment);
         // Now you have the payment details,
         // you can store the authorization_code in your db to allow for recurrent subscriptions
         // you can then redirect or do whatever you want
-
+        $baseUrl = "https://api.paystack.co";
         if(Session::has('payment_type')){
             if(Session::get('payment_type') == 'cart_payment'){
+                $paystack = new Paystack(env('PAYSTACK_SECRET_KEY'), $baseUrl);
+                $payment = $paystack->getPaymentData();
+                $payment_detalis = json_encode($payment);
                 $checkoutController = new CheckoutController;
                 return $checkoutController->checkout_done(Session::get('order_id'), $payment_detalis);
             }
             elseif (Session::get('payment_type') == 'seller_payment') {
+                $seller = Seller::findOrFail(Session::get('payment_data')['seller_id']);
+                $paystack = new Paystack($seller->paystack_secret_key, $baseUrl);
+                $payment = $paystack->getPaymentData();
+                $payment_detalis = json_encode($payment);
                 $commissionController = new CommissionController;
                 return $commissionController->seller_payment_done(Session::get('payment_data'), $payment_detalis);
             }
             elseif (Session::get('payment_type') == 'wallet_payment') {
+                $paystack = new Paystack(env('PAYSTACK_SECRET_KEY'), $baseUrl);
+                $payment = $paystack->getPaymentData();
+                $payment_detalis = json_encode($payment);
                 $walletController = new WalletController;
                 return $walletController->wallet_payment_done(Session::get('payment_data'), $payment_detalis);
             }
